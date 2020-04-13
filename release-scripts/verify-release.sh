84a78ac9410d52f1226fd36434df5302e72db13a
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-13 14:45:42 -0400

    fix job deps

---
+++ release-scripts/verify-release.sh
@@ -0,0 +1,15 @@
+set -eox
+curl https://api.github.com/repos/returntocorp/sgrep/releases/latest > release.json
+
+# Look for release notes
+cat release.json | jq '.body' | grep "Changed"
+
+cat release.json | jq -r '.tag_name' | sed 's/^v//' > version
+cat release.json | jq -r '.assets[].name'
+echo "Looking for version: $(cat version)"
+
+# Look for ubuntu binary
+cat release.json | jq '.assets[].name' | grep "sgrep-$(cat version)-ubuntu-16.04.tgz"
+
+# Look for OSX binary
+cat release.json | jq '.assets[].name' | grep "sgrep-$(cat version)-osx.zip"
